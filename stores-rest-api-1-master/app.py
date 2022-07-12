f8122976385434e754c5f0d7ec8843ff246332c2
kikigigi <momokocute1@gmail.com>
2022-07-12 10:42:55 +0100

    Update app.py

--- stores-rest-api-1-master/app.py
+++ stores-rest-api-1-master/app.py
@@ -7,7 +7,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from resources.store import Store, StoreList
+from resources.store import Store, Stores
 
 app = Flask(__name__)
 
